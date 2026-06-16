8e4ec5f62395f85cfc0168194826c9242832e090
Tich0ndrius <tykhon.nest@ukr.net>
2026-06-16 19:12:42 +0300

    Initial Commit

---
+++ Recamgnition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recamgnition
+//
+//  Created by Tykhon on 16.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recamgnition/RecamgnitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecamgnitionApp.swift
+//  Recamgnition
+//
+//  Created by Tykhon on 16.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct RecamgnitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
